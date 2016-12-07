dbe84a7d7b8872d42c827361c2c879907bbe3dc5
Sergey Vlasov <sigprof@gmail.com>
2016-12-07 08:40:46 +0300

    build: Invoke jack-admin server-gc after every build
    Change-Id: Ie9980fc3df4a60137e12129300579a266e049a1b
    Signed-off-by: Sergey Vlasov <sigprof@gmail.com>

--- core/definitions.mk
+++ core/definitions.mk
@@ -2152,6 +2152,7 @@ $(call call-jack) \
     $(if $(PRIVATE_JACK_PROGUARD_FLAGS),--config-proguard $@.flags) \
     $$tmpEcjArg \
     || ( rm -rf $(PRIVATE_CLASSES_JACK); exit 41 )
+$(hide) $(JACK)-admin server-gc
 $(hide) mv $(PRIVATE_JACK_INTERMEDIATES_DIR)/classes*.dex $(dir $@)
 $(hide) rm -f $(PRIVATE_JACK_INTERMEDIATES_DIR)/java-source-list
 $(if $(PRIVATE_EXTRA_JAR_ARGS),$(hide) rm -rf $@.res.tmp)
@@ -2191,6 +2192,7 @@ $(hide) if [ -s $@.java-source-list-uniq ] ; then \
 	    $(if $(PRIVATE_JACK_INCREMENTAL_DIR),--incremental-folder $(PRIVATE_JACK_INCREMENTAL_DIR)) \
 	    @$@.java-source-list-uniq; \
 fi
+$(hide) $(JACK)-admin server-gc
 touch $@
 endef
 
@@ -2207,6 +2209,7 @@ define transform-jar-to-jack
 	    --import $< \
 	    --import-resource $@.tmpjill.res \
 	    --output-jack $@
+	$(hide) $(JACK)-admin server-gc
 	$(hide) rm -rf $@.tmpjill.res
 endef
 
@@ -2326,6 +2329,7 @@ $(call call-jack) \
     $(if $(PRIVATE_JACK_PROGUARD_FLAGS),--config-proguard $@.flags) \
     $$tmpEcjArg \
     || ( rm -f $@ ; exit 41 )
+$(hide) $(JACK)-admin server-gc
 $(hide) rm -f $(PRIVATE_JACK_INTERMEDIATES_DIR)/java-source-list
 $(if $(PRIVATE_EXTRA_JAR_ARGS),$(hide) rm -rf $@.res.tmp)
 $(hide) mv $(PRIVATE_JACK_INTERMEDIATES_DIR)/java-source-list-uniq $(PRIVATE_JACK_INTERMEDIATES_DIR).java-source-list
